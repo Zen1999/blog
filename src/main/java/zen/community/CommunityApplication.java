186679ed99a4fbb00bf4bd741bd2b376312a7789
Zen1999 <626017607@qq.com>
2020-05-12 01:36:09 +0800

    init repo

---
+++ src/main/java/zen/community/CommunityApplication.java
@@ -0,0 +1,15 @@
+package zen.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+  public static void main(String[] args) {
+    // Spring 通过 ioc容器 来管理 Bean，
+    // 只要在 SpringBootApplication 同一级或下一级目录下 带注解的组件都会加载进来-
+    SpringApplication.run(BlogApplication.class, args);
+  }
+
+}
